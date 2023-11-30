689199f281f624e24ef39862342db6c4dee87297
naybala <naybala.zotefamily@gmail.com>
2023-11-30 21:57:17 +0630

    update process

--- function/productEditFun.php
+++ function/productEditFun.php
@@ -3,6 +3,5 @@
 if (isset($_REQUEST['action'])) {
     $id = $_REQUEST['action'];
     $editProduct = mysqli_query($connection, "SELECT * FROM products where id = '" . $id . "'");
-
     $editProduct = mysqli_fetch_assoc($editProduct);
 }
